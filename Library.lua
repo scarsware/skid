7a855a4eb4d5dbe5646d3ce575ba7d3d0c7bdaff
scarsware <scars@drugsellers.com>
2025-02-23 18:28:13 -0600

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1254,7 +1254,7 @@ do
                 local Event;
                 Event = InputService.InputBegan:Connect(function(Input, gameProcessed)
                     if not gameProcessed then return end
-                        local Key;
+                    local Key;
 
                     if Input.UserInputType == Enum.UserInputType.Keyboard then
                         Key = Input.KeyCode.Name;
@@ -3604,6 +3604,7 @@ function Library:CreateWindow(...)
     end
 
     Library:GiveSignal(InputService.InputBegan:Connect(function(Input, Processed)
+        if Processed then return end
         if type(Library.ToggleKeybind) == 'table' and Library.ToggleKeybind.Type == 'KeyPicker' then
             if Input.UserInputType == Enum.UserInputType.Keyboard and Input.KeyCode.Name == Library.ToggleKeybind.Value then
                 task.spawn(Library.Toggle)
